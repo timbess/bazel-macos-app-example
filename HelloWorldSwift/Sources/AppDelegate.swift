572f97662da38a3e56a7b665f9e8c6f731727d59
Timothy Bess <timbessmail@gmail.com>
2024-06-02 03:51:40 -0400

    cleanup

--- HelloWorldSwift/Sources/AppDelegate.swift
+++ HelloWorldSwift/Sources/AppDelegate.swift
@@ -12,14 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//import Cocoa
-
-//@NSApplicationMain
-//class AppDelegate: NSObject, NSApplicationDelegate {
-//}
-
-
-import Foundation
 import SwiftUI
 
 @main
